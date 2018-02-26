b158a5fa289102af8ddf639c120ecaa54f05f640
Andy <sgt.a.smith@gmail.com>
2018-02-25 16:54:40 -0800

    a

--- bash_launch_scripts/git_upload.sh
+++ bash_launch_scripts/git_upload.sh
@@ -2,12 +2,12 @@
 
 reason=1
 sleep_time='5s'
-commit_time='5s'
-status_time='5s'
+commit_time='1s'
+status_time='1s'
  
 my_pid=$$
 
-echo "pushing costmap_bridge"
+echo "pushing costmap_bridge**********************************************************************"
 cd ~/catkin_ws/src/costmap_bridge &
 git status &
 sleep $status_time
@@ -17,7 +17,7 @@ sleep $commit_time
 git push &
 sleep $sleep_time
 
-echo "pushing custom messages"
+echo "pushing custom messages**********************************************************************"
 cd ~/catkin_ws/src/custom_messages &
 git status &
 git add -A &
@@ -25,7 +25,7 @@ git commit -m "$reason" &
 git push &
 sleep $sleep_time
 
-echo "pushing dmcts"
+echo "pushing dmcts**********************************************************************"
 cd ~/catkin_ws/src/dmcts &
 git status &
 git add -A &
@@ -33,7 +33,7 @@ git commit -m "$reason" &
 git push &
 sleep $sleep_time
 
-echo "pushing dmcts_world $reason"
+echo "pushing dmcts_world **********************************************************************"
 cd ~/catkin_ws/src/dmcts_world &
 git status &
 git add -A &
@@ -41,7 +41,7 @@ git commit -m "$reason" &
 git push &
 sleep $sleep_time
 
-echo "pushing fake dmcts quad"
+echo "pushing fake dmcts quad**********************************************************************"
 cd ~/catkin_ws/src/fake_dmcts_quad &
 git status &
 git add -A &
@@ -49,7 +49,7 @@ git commit -m "$reason" &
 git push &
 sleep $sleep_time
 
-echo "pushing gps_to_local_bridge"
+echo "pushing gps_to_local_bridge**********************************************************************"
 cd ~/catkin_ws/src/gps_to_local_bridge &
 git status &
 git add -A &
@@ -57,7 +57,7 @@ git commit -m "$reason" &
 git push &
 sleep $sleep_time
 
-echo "pushing my_quad_controller"
+echo "pushing my_quad_controller**********************************************************************"
 cd ~/catkin_ws/src/my_quad_controller &
 git status &
 git add -A &
@@ -65,7 +65,7 @@ git commit -m "$reason" &
 git push &
 sleep $sleep_time
 
-echo "pushing xbee_bridge"
+echo "pushing xbee_bridge**********************************************************************"
 cd ~/catkin_ws/src/xbee_bridge &
 git status &
 git add -A &
