e2b2273cbb869b4ff597bf763cd7f6428dec1a2e
Andy <sgt.a.smith@gmail.com>
2018-02-25 16:29:14 -0800

    bash launchers

--- bash_launch_scripts/n_controlled_fake_quads.sh
+++ bash_launch_scripts/n_controlled_fake_quads.sh
@@ -8,6 +8,7 @@ score_run=false
 gazebo_obstacles=false
 p_task_initially_active=0.40
 cruising_speed=2.0
+use_xbee=false
  
 my_pid=$$
 echo "My process ID is $my_pid"
@@ -36,10 +37,10 @@ echo "setting sim_time true"
 #sleep 1s
 
 
-#echo "launching rviz"
-#rviz &
-#pid="$pid $!"
-#sleep 10s
+echo "launching rviz"
+rviz &
+pid="$pid $!"
+sleep 10s
 
 echo "launching dmcts fake quad nodes"
 for ((ai=0; ai<n_agents; ai++))
@@ -50,14 +51,14 @@ do
 done
 
 echo "launching dmcts_world_node"
-roslaunch dmcts_world dmcts_world.launch num_agents:=$n_agents num_nodes:=$n_nodes param_number:=$param coord_method:=$coord_method  score_run:=$score_run gazebo_obstacles:=$gazebo_obstacles p_task_initially_active:=$p_task_initially_active &
+roslaunch dmcts_world dmcts_world.launch num_agents:=$n_agents num_nodes:=$n_nodes param_number:=$param coord_method:=$coord_method  score_run:=$score_run gazebo_obstacles:=$gazebo_obstacles p_task_initially_active:=$p_task_initially_active use_xbee:=$use_xbee &
 pid="$pid $!"
 sleep 1s
 
 echo "launching dmcts quad nodes"
 for ((ai=0; ai<n_agents; ai++))
 do
-    roslaunch dmcts dmcts_n.launch agent_index:=$ai num_agents:=$n_agents num_nodes:=$n_nodes coord_method:=$coord_method  desired_altitude:=${zs[ai]} p_task_initially_active:=$p_task_initially_active pay_obstacle_costs:=${pay_obs_costs[ai]} not_simulation:=false cruising_speed:=$cruising_speed & 
+    roslaunch dmcts dmcts_n.launch agent_index:=$ai num_agents:=$n_agents num_nodes:=$n_nodes coord_method:=$coord_method  desired_altitude:=${zs[ai]} p_task_initially_active:=$p_task_initially_active pay_obstacle_costs:=${pay_obs_costs[ai]} not_simulation:=false cruising_speed:=$cruising_speed cruising_speed:=$cruising_speed param_number:=$param use_xbee:=$use_xbee & 
     #roslaunch dmcts dmcts_n.launch agent_index:=$ai num_agents:=$n_agents num_nodes:=$n_nodes coord_method:='greedy_completion_reward' &
     #roslaunch dmcts dmcts_n.launch agent_index:=$ai num_agents:=$n_agents num_nodes:=$n_nodes coord_method:='mcts_task_by_completion_reward' &
     #roslaunch dmcts dmcts_n.launch agent_index:=$ai num_agents:=$n_agents num_nodes:=$n_nodes coord_method:='mcts_task_by_completion_reward_impact_optimal' &
--- bash_launch_scripts/n_controlled_quads.sh
+++ bash_launch_scripts/n_controlled_quads.sh
@@ -7,6 +7,8 @@ coord_method=$4
 score_run=true
 gazebo_obstacles=false
 p_task_initially_active=0.625
+cruising_speed=2.0
+use_xbee=false
  
 my_pid=$$
 echo "My process ID is $my_pid"
@@ -42,20 +44,20 @@ do
     sleep 5s
 done 
 
-#echo "launching rviz"
-#rviz &
-#pid="$pid $!"
-#sleep 10s
+echo "launching rviz"
+rviz &
+pid="$pid $!"
+sleep 10s
 
 echo "launching dmcts_world_node"
-roslaunch dmcts_world dmcts_world.launch num_agents:=$n_agents num_nodes:=$n_nodes param_number:=$param coord_method:=$coord_method  score_run:=$score_run gazebo_obstacles:=$gazebo_obstacles p_task_initially_active:=$p_task_initially_active &
+roslaunch dmcts_world dmcts_world.launch num_agents:=$n_agents num_nodes:=$n_nodes param_number:=$param coord_method:=$coord_method  score_run:=$score_run gazebo_obstacles:=$gazebo_obstacles p_task_initially_active:=$p_task_initially_active use_xbee:=$use_xbee&
 pid="$pid $!"
 sleep 5s
 
 echo "launching dmcts quad nodes"
 for ((ai=0; ai<n_agents; ai++))
 do
-    roslaunch dmcts dmcts_n.launch agent_index:=$ai num_agents:=$n_agents num_nodes:=$n_nodes coord_method:=$coord_method  desired_altitude:=${zs[ai]} p_task_initially_active:=$p_task_initially_active pay_obstacle_costs:=${pay_obs_costs[ai]} not_simulation:=true & 
+    roslaunch dmcts dmcts_n.launch agent_index:=$ai num_agents:=$n_agents num_nodes:=$n_nodes coord_method:=$coord_method  desired_altitude:=${zs[ai]} p_task_initially_active:=$p_task_initially_active pay_obstacle_costs:=${pay_obs_costs[ai]} not_simulation:=true cruising_speed:=$cruising_speed param_number:=$param use_xbee:=$use_xbee & 
     #roslaunch dmcts dmcts_n.launch agent_index:=$ai num_agents:=$n_agents num_nodes:=$n_nodes coord_method:='greedy_completion_reward' &
     #roslaunch dmcts dmcts_n.launch agent_index:=$ai num_agents:=$n_agents num_nodes:=$n_nodes coord_method:='mcts_task_by_completion_reward' &
     #roslaunch dmcts dmcts_n.launch agent_index:=$ai num_agents:=$n_agents num_nodes:=$n_nodes coord_method:='mcts_task_by_completion_reward_impact_optimal' &
