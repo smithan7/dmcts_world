5f91412ce8efb989761831ea30a4ac37acf5ce86
Andy <sgt.a.smith@gmail.com>
2018-03-15 10:47:08 -0700

    post first field trial

---
+++ bash_launch_scripts/n_controlled_simulated_quads_params.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+n_agents=$1
+n_nodes=$2
+param=$3
+coord_method=$4
+p_task_initially_active=$5
+alpha=$6
+beta=$7
+depth=$8
+epsilon=0.4702117011
+min_sampling_threshold=0.240479139
+
+score_run=true
+gazebo_obstacles=false
+cruising_speed=1.0
+use_xbee=false
+use_gazebo=false
+end_time=60.0
+way_point_tol=0.1
+use_hector_quad=false
+world_display_map=true
+agent_display_map=false
+hardware_trial=true # more, is this a search and resuce mission
+n_agent_types=2
+n_task_types=2
+flat_tasks=true
+speed_penalty=0.0
+
+my_pid=$$
+echo "My process ID is $my_pid"
+echo "  n_agents: $n_agents"
+echo "  n_nodes: $n_nodes"
+echo "  param: $param"
+echo "  coord_method: $coord_method"
+echo "  p_task_initially_active: $p_task_initially_active"
+echo "  min_sampling_threshold: $min_sampling_threshold"
+echo "  way_point_tol: $way_point_tol"
+
+echo "Sourcing ~/catkin_ws/devel/setup.bash"
+source ~/catkin_ws/devel/setup.bash &
+pid=$!
+
+echo "Launching roscore..."
+roscore &
+pid="pid $!"
+sleep 1s
+
+# Load specific stuff for this trial
+echo "Loading ROS params"
+#rosparam load "$(rospack find dmcts_world)/bash_launch_scripts/launch_params/osu_field_params.yaml"
+#rosparam load "$(rospack find dmcts_world)/bash_launch_scripts/launch_params/willamette_park_params.yaml"
+rosparam load "$(rospack find dmcts_world)/bash_launch_scripts/launch_params/gazebo_map_params.yaml" &
+#rosparam load "$(rospack find dmcts_world)/bash_launch_scripts/launch_params/dmcts_params.yaml"
+rosparam set "/alpha" $alpha
+rosparam set "/beta" $beta
+rosparam set "/depth" $depth
+rosparam set "/epsilon" $epsilon
+rosparam set "/min_sampling_threshold" $min_sampling_threshold
+
+rosparam set "/param_number" $param
+rosparam set "/end_time" $end_time
+rosparam set "/way_point_tol" $way_point_tol
+rosparam set "/use_gazebo_obstacles" $gazebo_obstacles
+rosparam set "/p_task_initially_active" $p_task_initially_active
+rosparam set "/number_of_nodes" $n_nodes
+rosparam set "/number_of_agents" $n_agents
+rosparam set "/coord_method" $coord_method
+rosparam set "/world_directory" "$(rospack find dmcts_world)/"
+rosparam set "/score_run" $score_run
+rosparam set "/dmcts_world/display_map" $world_display_map
+rosparam set "/agent_display_map" $agent_display_map
+rosparam set "/use_gazebo" $use_gazebo
+rosparam set "/hardware_trial" $hardware_trial
+rosparam set "/flat_tasks" $flat_tasks
+rosparam set "/speed_penalty" $speed_penalty
+rosparam set "/n_task_types" $n_task_types
+rosparam set "/n_agent_types" $n_agent_types
+echo "Loaded ROS params"
+sleep 2s
+
+
+echo "launching dmcts fake quad nodes"
+for ((ai=0; ai<n_agents; ai++))
+do
+    roslaunch fake_dmcts_quad fake_dmcts_quad.launch agent_index:=$ai &
+    pid="$pid $!"
+    sleep 1s
+done
+
+#echo "launching rviz"
+#rviz &
+#pid="$pid $!"
+#sleep 10s
+
+echo "launching dmcts quad nodes"
+for ((ai=0; ai<n_agents; ai++))
+do
+    roslaunch dmcts dmcts_n.launch agent_index:=$ai use_xbee:=$use_xbee use_hector_quad:=$use_hector_quad &
+    pid="$pid $!" 
+    sleep 5s
+done
+
+echo "launching dmcts_world_node"
+roslaunch dmcts_world dmcts_world.launch use_xbee:=$use_xbee
+pid="$pid $!"
+sleep 1s
---
+++ worlds/dmcts_5_agent_trainer.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+import csv
+import random
+import os
+
+if __name__ == '__main__':
+
+    num_nodes = 100
+    coord_type_set = ['"mcts_task_by_completion_reward_gradient"']#, '"greedy_completion_reward"']
+
+    num_agents = 5
+    coord_type = "mcts_task_by_completion_reward_gradient"
+    p_active = 0.5
+
+    use_gazebo = False
+    maps_per_round = 3
+
+    depth_set = [4,5,6,7,8]
+    beta_set = [0.5, 1.0, 1.5]
+    alpha_set = [0.1, 0.25, 0.5, 0.75, 0.9, 0.95, 0.99, 1.0]
+    param_set = [34, 50, 81]
+
+    for depth in depth_set:
+        for beta in beta_set:
+            for alpha in alpha_set:
+                
+                os.system('rm /home/andy/catkin_ws/src/dmcts_world/worlds/results.csv')
+                os.system('touch /home/andy/catkin_ws/src/dmcts_world/worlds/results.csv')
+
+
+                for param in param_set:
+                    if use_gazebo:
+                        command = '/home/andy/catkin_ws/src/dmcts_world/bash_launch_scripts/n_controlled_quads.sh ' + str(num_agents) + ' ' + str(num_nodes) + ' ' + str(param) + ' ' + coord_type + ' ' + str(p_active)
+                    else:
+                        command = '/home/andy/catkin_ws/src/dmcts_world/bash_launch_scripts/n_controlled_simulated_quads_params.sh ' + str(num_agents) + ' ' + str(num_nodes) + ' ' + str(param) + ' ' + coord_type + ' ' + str(p_active) + ' ' + str(alpha) + ' ' + str(beta) + ' ' + str(depth) 
+                        #command = '/home/andy/catkin_ws/src/dmcts_world/bash_launch_scripts/n_controlled_fake_quads_params.sh 2 100 ' + str(iter) + ' "greedy_completion_reward" 0.25'
+        			
+                    print("sending bash command: " + command)
+            
+                    os.system(command)
+                    os.system('sleep 5s')
+                    os.system('rosnode kill -a')
+                    os.system('sleep 5s')
+    
+                results = []
+                n_rows = 0.0
+                test_score = 0.0
+                with open('/home/andy/catkin_ws/src/dmcts_world/worlds/results.csv', 'rb') as resultsfile:
+                    r = csv.reader(resultsfile)
+                    for i, row in enumerate(r,1):
+                        test_score += float(row[0])
+                        results.append(float(row[0]))
+                        n_rows += 1.0
+                        
+                print("Results found: " + str(n_rows))
+                test_score /= n_rows
+        
+                os.system('rm /home/andy/catkin_ws/src/dmcts_world/worlds/results.csv')
+        
+                print("test_values mean score: " + str(test_score))
+                
+                with open('/home/andy/catkin_ws/src/dmcts_world/worlds/5_agent_trainer_results.csv', 'a') as csvfile:
+                    w= csv.writer(csvfile, delimiter=',')
+                    tt = [str(test_score), str(alpha), str(beta), str(depth), results]
+                    w.writerow(tt)
