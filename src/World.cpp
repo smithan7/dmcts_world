c6fd9598046704fe31fdb5035aed5bf2e0dba3fd
Andy <sgt.a.smith@gmail.com>
2018-04-01 13:14:14 -0700

    output time in print statement

--- src/World.cpp
+++ src/World.cpp
@@ -428,16 +428,16 @@ void World::request_work_callback(const custom_messages::DMCTS_Request_Work &msg
 
                 if(this->nodes[msg.n_index]->is_active()){
                     if (work_done > 0.0){
-		                ROS_INFO("Agent[%i] worked: %0.2f (u) on task[%i]", msg.a_index, work_done, msg.n_index);
+		                ROS_INFO("Time(%0.2f):Agent[%i] worked: %0.2f (u) on task[%i]", this->c_time, msg.a_index, work_done, msg.n_index);
                         complete = 1; // Succesfully did some work, but task is still active
                     }
                     else{
-		                ROS_WARN("Agent[%i] FAILED to work on task[%i]", msg.a_index, msg.n_index);
+		                ROS_WARN("Time(%0.2f):Agent[%i] FAILED to work on task[%i]", this->c_time, msg.a_index, msg.n_index);
                         complete = -1; // Task is active and I did no work
                     }
                 }
                 else{
-                	ROS_INFO("Agent[%i] completed task[%i] and collected %0.2f", msg.a_index, msg.n_index, reward_collected);
+                	ROS_INFO("Time(%0.2f):Agent[%i] completed task[%i] and collected %0.2f", this->c_time, msg.a_index, msg.n_index, reward_collected);
                     complete = 0; // Task is now complete
                 }
 
